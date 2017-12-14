e6333ad214801558567bc55f462b48059d5aa3a5
Paulius <interro@gmail.com>
2017-12-14 18:40:48 +0200

    test for signed_payload endpoint

---
+++ tests/test_signed_payload.py
@@ -0,0 +1,46 @@
+import requests
+from eth_keys import keys
+import json
+import base64
+
+
+def print_payload_signature():
+    pk = keys.PrivateKey(b'\x01' * 32)
+
+    payload = {
+        "identity": pk.public_key.to_checksum_address().lower(),
+        "service_proposal": {
+            "id": 1,
+            "format": "service-proposal/v1",
+            "provider_id": "node1",
+        }
+    }
+
+    signature = pk.sign_msg(json.dumps(payload))
+    print json.dumps(payload)
+    print pk.public_key.to_checksum_address().lower()
+    print base64.b64encode(signature.to_bytes())
+
+
+def test_signed_payload():
+    payload = {
+        "identity": "0x1a642f0e3c3af545e7acbd38b07251b3990914f1",
+        "service_proposal": {
+            "id": 1,
+            "format": "service-proposal/v1",
+            "provider_id": "node1",
+        }
+    }
+
+    headers = {
+        'signature': "nXA/isHCIQsAUqmPodf9ZsDdPYYmV+Z+poBg45d3jp4Ww0wmuqAI7VmXib69J3WvglRwT7kvoHkInZ+YO+vy3gE="
+    }
+
+    re = requests.post(
+        'http://127.0.0.1:5000/v1/signed_payload',
+        data=json.dumps(payload),
+        headers=headers
+    )
+    print re.content
+
+test_signed_payload()
