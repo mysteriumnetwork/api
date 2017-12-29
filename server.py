8b58910c224291ef71fd5c964440d35cc7e97087
Donatas Kucinskas <donce.lt@gmail.com>
2017-12-29 15:50:53 +0200

    Add server.py for running app in production mode using "tornado"

---
+++ server.py
@@ -0,0 +1,10 @@
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from app import app, db
+
+db.init_app(app)
+
+http_server = HTTPServer(WSGIContainer(app))
+http_server.listen(5000)
+IOLoop.instance().start()
