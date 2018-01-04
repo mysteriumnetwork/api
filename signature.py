19b627ed927e132b3f7bebdd50e8a211084d3251
Donatas Kucinskas <donce.lt@gmail.com>
2018-01-04 17:24:11 +0200

    Fix some of linter warning

--- signature.py
+++ signature.py
@@ -10,10 +10,12 @@ from eth_keys.utils.address import (
     public_key_bytes_to_address,
 )
 
+
 def recover_public_key(message, signature_bytes):
     signature = Signature(signature_bytes)
     public_key = signature.recover_public_key_from_msg(message)
     return public_key
+
 
 def recover_public_address(message, signature_bytes):
     public_key = recover_public_key(message, signature_bytes)
