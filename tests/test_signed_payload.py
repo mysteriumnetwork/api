6ca45bac13b6b1348b66e79891701969a12f6b21
Paulius <interro@gmail.com>
2018-01-11 15:43:13 +0200

    generate_test_authorization

--- tests/test_signed_payload.py
+++ tests/test_signed_payload.py
@@ -1,21 +1,16 @@
 from tests.test_case import TestCase
-import base64
-from tests.utils import sign_message_with_static_key
+from tests.utils import generate_test_authorization
 
 
 class TestApi(TestCase):
     def test_successful_request(self):
-        signature, public_address = sign_message_with_static_key('')
-        headers = {
-            "Authorization": "Signature {}".format(base64.b64encode(signature))
-        }
-
-        re = self.client.get('/v1/me', headers=headers)
+        auth = generate_test_authorization()
+        re = self.client.get('/v1/me', headers=auth['headers'])
 
         self.assertEqual(200, re.status_code)
         self.assertEqual(
             re.json,
-            {'identity': public_address.lower()}
+            {'identity': auth['public_address']}
         )
 
     def test_failure(self):
--- tests/utils.py
+++ tests/utils.py
@@ -1,4 +1,5 @@
 from eth_keys import keys
+import base64
 
 
 def sign_message_with_static_key(message):
@@ -7,3 +8,14 @@ def sign_message_with_static_key(message):
     signature_bytes = signature.to_bytes()
     public_address = pk.public_key.to_checksum_address()
     return signature_bytes, public_address
+
+def generate_test_authorization(message=''):
+    signature, public_address = sign_message_with_static_key(message)
+    headers = {
+        "Authorization": "Signature {}".format(base64.b64encode(signature))
+    }
+
+    return {
+        'headers': headers,
+        'public_address': public_address.lower()
+    }
