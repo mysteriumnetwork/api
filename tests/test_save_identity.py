d257e8443df7ea0f591afbf238775d74f1e48c88
Paulius <interro@gmail.com>
2018-01-12 16:14:48 +0200

    test identity already exists

--- tests/test_save_identity.py
+++ tests/test_save_identity.py
@@ -1,6 +1,6 @@
 import unittest
 import json
-from tests.test_case import TestCase
+from tests.test_case import TestCase, db
 from tests.utils import generate_test_authorization
 from models import Identity
 
@@ -15,11 +15,25 @@ class TestSaveIdentity(TestCase):
             headers=auth['headers']
         )
         self.assertEqual(200, re.status_code)
+        self.assertEqual({}, re.json)
 
         identity_record = Identity.query.get(auth['public_address'])
         self.assertIsNotNone(identity_record)
 
-    # TODO: test failure scenarios
+    def test_failure_identity_already_exists(self):
+        payload = json.dumps({})
+        auth = generate_test_authorization(payload)
+
+        identity_record = Identity(auth['public_address'])
+        db.session.add(identity_record)
+
+        re = self.client.post(
+            '/v1/identities',
+            data=payload,
+            headers=auth['headers']
+        )
+        self.assertEqual(400, re.status_code)
+        self.assertEqual({"error": 'identity already exists'}, re.json)
 
 
 if __name__ == '__main__':
