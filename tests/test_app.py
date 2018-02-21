27febe90782934842fbb3cc848a0aaa40a7868a3
Paulius <interro@gmail.com>
2018-02-21 11:19:49 +0200

    try-finally

--- tests/test_app.py
+++ tests/test_app.py
@@ -364,44 +364,48 @@ class TestApi(TestCase):
 
     def test_restrict_by_ip_fail(self):
         settings.RESTRICT_BY_IP_ENABLED = True
-        settings.ALLOWED_IP_ADDRESSES = [
-            '1.1.1.1',
-            '2.2.2.2',
-        ]
-        payload = {}
-        auth = generate_test_authorization(json.dumps(payload))
+        try:
+            settings.ALLOWED_IP_ADDRESSES = [
+                '1.1.1.1',
+                '2.2.2.2',
+            ]
+            payload = {}
+            auth = generate_test_authorization(json.dumps(payload))
 
-        self._create_node(auth['public_address'])
+            self._create_node(auth['public_address'])
 
-        re = self._post(
-            '/v1/ping_proposal',
-            payload,
-            headers=auth['headers']
-        )
-        self.assertEqual(403, re.status_code)
-        self.assertEqual({'error': 'resource is forbidden'}, re.json)
-        settings.RESTRICT_BY_IP_ENABLED = False
+            re = self._post(
+                '/v1/ping_proposal',
+                payload,
+                headers=auth['headers']
+            )
+            self.assertEqual(403, re.status_code)
+            self.assertEqual({'error': 'resource is forbidden'}, re.json)
+        finally:
+            settings.RESTRICT_BY_IP_ENABLED = False
 
     def test_restrict_by_ip_success(self):
         settings.RESTRICT_BY_IP_ENABLED = True
-        settings.ALLOWED_IP_ADDRESSES = [
-            '1.1.1.1',
-            '2.2.2.2',
-            self.REMOTE_ADDR
-        ]
-        payload = {}
-        auth = generate_test_authorization(json.dumps(payload))
+        try:
+            settings.ALLOWED_IP_ADDRESSES = [
+                '1.1.1.1',
+                '2.2.2.2',
+                self.REMOTE_ADDR
+            ]
+            payload = {}
+            auth = generate_test_authorization(json.dumps(payload))
 
-        self._create_node(auth['public_address'])
+            self._create_node(auth['public_address'])
 
-        re = self._post(
-            '/v1/ping_proposal',
-            payload,
-            headers=auth['headers']
-        )
-        self.assertEqual(200, re.status_code)
-        self.assertEqual({}, re.json)
-        settings.RESTRICT_BY_IP_ENABLED = False
+            re = self._post(
+                '/v1/ping_proposal',
+                payload,
+                headers=auth['headers']
+            )
+            self.assertEqual(200, re.status_code)
+            self.assertEqual({}, re.json)
+        finally:
+            settings.RESTRICT_BY_IP_ENABLED = False
 
     def _get(self, url, params={}):
         return self.client.get(
