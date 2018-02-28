cdc4b688e766e76cb9a2b0e735c1f26185392b56
Paulius <interro@gmail.com>
2018-02-28 15:47:26 +0200

    test for session endpoint

---
+++ dashboard/tests/test_app.py
@@ -0,0 +1,13 @@
+from test_case import TestCase
+
+
+class TestEndpoints(TestCase):
+    def test_sessions(self):
+        re = self._get('/sessions')
+        self.assertEqual(200, re.status_code)
+
+    def _get(self, url, params={}):
+        return self.client.get(
+            url,
+            query_string=params,
+        )
