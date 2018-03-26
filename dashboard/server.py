5e410b5f2ed0622462da8f65828574349571187a
interro <interro@gmail.com>
2018-03-26 17:19:19 +0300

    moved append path to server.py

--- dashboard/server.py
+++ dashboard/server.py
@@ -1,3 +1,7 @@
+import sys
+from os import path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))  # noqa
+
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
