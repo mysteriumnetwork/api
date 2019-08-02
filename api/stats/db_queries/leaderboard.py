f24377f068127c798fa75472e038cc29ee9a0307
interro <interro@gmail.com>
2019-08-02 16:45:24 +0300

    change leaderboard query

--- api/stats/db_queries/leaderboard.py
+++ api/stats/db_queries/leaderboard.py
@@ -5,24 +5,27 @@ from models import db, Node
 
 
 class LeaderboardRow:
-    def __init__(self, provider_id, sessions_count, total_bytes, unique_users):
+    def __init__(
+            self, provider_id, sessions_count,
+            total_bytes, unique_users, country, service_type):
         self.provider_id = provider_id
         self.sessions_count = sessions_count
         self.total_bytes = total_bytes
         self.unique_users = unique_users
+        self.country = country
+        self.service_type = service_type
         self.availability = None
-        self.data_transferred = None
-        self.country = None
         self.service_status = None
 
     @staticmethod
     def from_sql_row(row):
-        provider_id, sessions_count, total_bytes, unique_users = row
         return LeaderboardRow(
-            provider_id,
-            sessions_count,
-            total_bytes,
-            unique_users
+            row['provider_id'],
+            row['sessions_count'],
+            row['total_bytes'],
+            row['unique_users'],
+            row['country'].replace('"', ''),
+            row['service_type']
         )
 
     def __repr__(self):
@@ -38,27 +41,51 @@ class LeaderboardRow:
 
 def get_leaderboard_rows(date_from, date_to, offset=0, limit=10):
     query = text("""
-        SELECT ir.identity AS provider_id,
-        COUNT(s.session_key) AS sessions_count,
-        SUM(s.client_bytes_sent) + SUM(s.client_bytes_received) AS total_bytes,
-        COUNT(DISTINCT(s.consumer_id)) AS unique_users
+        SELECT
+            n.node_type,
+            n.service_type,
+            ir.identity AS provider_id,
+            n.updated_at as node_updated_at,
+            COUNT(s.session_key) AS sessions_count,
+            COUNT(DISTINCT(s.consumer_id)) AS unique_users,
+            (
+                SUM(s.client_bytes_sent) + SUM(s.client_bytes_received)
+            ) AS total_bytes,
+            JSON_EXTRACT(
+                n.proposal, "$.service_definition.location.country"
+            ) AS country
         FROM identity_registration ir
         INNER JOIN node n ON n.node_key = ir.identity
         INNER JOIN session s ON s.node_key = ir.identity
             AND s.service_type = n.service_type
-        WHERE s.client_updated_at >= :date_from
-            AND s.client_updated_at <= :date_to
-            AND n.updated_at >= :date_from
-            AND n.updated_at <= :date_to
-            AND ir.bounty_program = TRUE
+        WHERE
+            s.client_updated_at BETWEEN :date_from AND :date_to
         GROUP BY ir.identity, n.node_key
+        HAVING
+            node_type = :node_type
+            AND service_type = :service_type
+            AND node_updated_at BETWEEN :date_from AND :date_to
+            AND (
+                country = 'US'
+                OR country = 'GB'
+                OR country = 'UK'
+                OR country = 'IT'
+            )
         ORDER BY total_bytes DESC
         LIMIT :limit
         OFFSET :offset
         """)
+
     rows = db.engine.execute(
-        query, date_from=date_from, date_to=date_to, limit=limit, offset=offset
+        query,
+        date_from=date_from,
+        date_to=date_to,
+        node_type='residential',
+        service_type='openvpn',
+        limit=limit,
+        offset=offset,
     ).fetchall()
+
     leader_board_rows = []
     for row in rows:
         leader_board_row = LeaderboardRow.from_sql_row(row)
@@ -74,13 +101,10 @@ def enrich_leaderboard_rows(rows, date_from, date_to):
             if node:
                 break
 
-        row.country = get_country_string(
-            node.get_country_from_service_proposal()
-        )
         row.service_status = get_node_status(node)
         hours_online = get_node_hours_online(
             row.provider_id,
-            node.service_type,
+            row.service_type,
             date_from,
             date_to
         )
@@ -91,4 +115,3 @@ def enrich_leaderboard_rows(rows, date_from, date_to):
         row.availability = '{0} / {1} h'.format(
             hours_online, total_hours_in_range
         )
-        row.data_transferred = row.total_bytes
